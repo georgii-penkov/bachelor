4954b1d7b832662b1fc57bbb40feaef8443395d8
G30RG3-P-KIP <georgy.penkov@gmail.com>
2025-04-09 17:21:49 +0200

    declared class for points and vectors TODO: define all the methods

--- learningroot/particles/threedgeometry.cpp
+++ learningroot/particles/threedgeometry.cpp
@@ -1,7 +1,74 @@
 #include <vector>
 #include <cmath>
 
-//class Point is ROOT::Math::XYZPoint
+const double THRESHOLD = 0.00001;
+class Cartesian;
+class Point;
+class Vector;
+
+class Cartesian
+{
+    private:
+        double m_x;
+        double m_y;
+        double m_z;
+    public: 
+        Cartesian(double, double, double);
+        double X() const {return m_x;};
+        double Y() const {return m_y;};
+        double Z() const {return m_z;};
+        void SetX(double x){m_x = x;};
+        void SetY(double y){m_y = y;};
+        void SetZ(double z){m_z = z;};
+
+        
+};
+Cartesian::Cartesian(double x = 0.0, double y = 0.0, double z = 0.0): m_x(x), m_y(y), m_z(z){};
+
+class Point : public Cartesian
+{
+    public:
+        Point operator+(const Vector&);
+        Vector operator-(const Point&);
+        void Draw() const;
+
+};
+
+
+class Vector : public Cartesian
+{
+    public:
+        Vector Cross(const Vector&);
+        double Dot(const Vector&);
+        Vector operator+(const Vector&);
+        Vector operator-(const Vector&);
+        Vector& operator*=(const double&);
+
+
+};
+
+Vector& Vector::operator*=(const double& scalar)
+{
+    this->SetX(this->X()*scalar);
+    this->SetY(this->Y()*scalar);
+    this->SetZ(this->Z()*scalar);
+    return(*this);
+};
+
+Vector operator*(const double& scalar, const Vector& vector)
+{
+    Vector result = vector;
+    result *= scalar;
+    return(result);
+};
+Vector operator*(const Vector& vector, const double& scalar)
+{
+    Vector result = vector;
+    result *= scalar;
+    return(result);
+};
+
+
 
 void RotatePoint(ROOT::Math::XYZPoint* point, double t_x, double t_y, double t_z)
 {
