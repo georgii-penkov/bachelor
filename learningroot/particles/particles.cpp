bcf846329d7d60c3253a60a9ef9807dfa4a41e2d
G30RG3-P-KIP <georgy.penkov@gmail.com>
2025-04-10 12:46:55 +0200

    fully working class of cartesian coordinates in threedgeometry to replace ROOT built-in replaced instances of ROOT coordinates in threedgeometry and particles

--- learningroot/particles/particles.cpp
+++ learningroot/particles/particles.cpp
@@ -12,9 +12,9 @@ const double RADIUS = tan(M_PI/2-(M_PI/14)) * 25.6/2 + 2.5;
 class Particle
 {
     private:
-        ROOT::Math::XYZPoint *m_position = new ROOT::Math::XYZPoint;
-        ROOT::Math::XYZPoint *m_previous_position = new ROOT::Math::XYZPoint;
-        ROOT::Math::XYZVector *m_velocity = new ROOT::Math::XYZVector;
+        Point *m_position = new Point;
+        Point *m_previous_position = new Point;
+        Vector *m_velocity = new Vector;
         bool m_alive;
         double m_created;
         double m_lifetime;
@@ -38,7 +38,7 @@ class Particle
             m_path->SetNextPoint(x, y, z);
             m_vector_marker->SetLineColor(kRed);
         };
-        Particle(ROOT::Math::XYZPoint start, ROOT::Math::XYZVector velocity, double created = 0)
+        Particle(Point start, Vector velocity, double created = 0)
         {
             m_position->SetCoordinates(start.X(), start.Y(), start.Z());
             m_previous_position->SetCoordinates(start.X(), start.Y(), start.Z());
@@ -53,17 +53,17 @@ class Particle
             m_vector_marker->SetLineColor(kRed);
         };
 
-        ROOT::Math::XYZPoint Position()
+        Point Position()
         {
             return(*m_position);
         };
 
-        ROOT::Math::XYZPoint *PositionPointer()
+        Point *PositionPointer()
         {
             return(m_position);
         }
 
-        ROOT::Math::XYZVector Velocity()
+        Vector Velocity()
         {
             return(*m_velocity);
         }
@@ -114,17 +114,17 @@ class Particle
             m_path->Draw();
         };
 
-        void Reflect(ROOT::Math::XYZVector *normal)
+        void Reflect(Vector *normal)
         {
-            ROOT::Math::XYZVector normed_normal = normal->Unit();
+            Vector normed_normal = normal->Unit();
             float scalar = m_velocity->Dot(normed_normal);
             if (scalar > 0) {scalar = -1*scalar;};
             *m_velocity = *m_velocity - (2*scalar) * normed_normal;
         };
 
-        ROOT::Math::XYZPoint Project(double time)
+        Point Project(double time)
         {
-            ROOT::Math::XYZPoint x;
+            Point x;
             x.SetCoordinates(m_position->X() + time * m_velocity->X(), m_position->Y() + time * m_velocity->Y(), m_position->Z() + time * m_velocity->Z());
             return(x);
         }
@@ -145,82 +145,76 @@ class Particle
 
 Threedpolygon* MakeBrick(double xwidth = 1, double ylength = 1, double zheight = 1, double xpos = 0, double ypos = 0, double zpos = 0)
 {
-    ROOT::Math::XYZPoint p1( 1, 0, 0);
-    p1 = p1*(xwidth/2);
-    ROOT::Math::XYZPoint p2( 0, 1, 0);
-    p2 = p2*(ylength/2);
-    ROOT::Math::XYZPoint p3( 0, 0, 1);
-    p3 = p3*(zheight/2);
-    ROOT::Math::XYZPoint p4(-1, 0, 0);
-    p4 = p4*(xwidth/2);
-    ROOT::Math::XYZPoint p5( 0,-1, 0);
-    p5 = p5*(ylength/2);
-    ROOT::Math::XYZPoint p6( 0, 0,-1);
-    p6 = p6*(zheight/2);
+    Point p1( xwidth/2, 0, 0);
+    Point p2( 0, ylength/2, 0);
+    Point p3( 0, 0, zheight/2);
+    Point p4(-xwidth/2, 0, 0);
+    Point p5( 0,-ylength/2, 0);
+    Point p6( 0, 0,-zheight/2);
 
-    ROOT::Math::XYZVector n1(-1, 0, 0);
-    ROOT::Math::XYZVector n2( 0,-1, 0);
-    ROOT::Math::XYZVector n3( 0, 0,-1);
-    ROOT::Math::XYZVector n4( 1, 0, 0);
-    ROOT::Math::XYZVector n5( 0, 1, 0);
-    ROOT::Math::XYZVector n6( 0, 0, 1);
+    Vector n1(-1, 0, 0);
+    Vector n2( 0,-1, 0);
+    Vector n3( 0, 0,-1);
+    Vector n4( 1, 0, 0);
+    Vector n5( 0, 1, 0);
+    Vector n6( 0, 0, 1);
     p1 = p1 + xpos*n4;
     p2 = p2 + ypos*n5;
     p3 = p3 + zpos*n6;
     p4 = p4 + xpos*n4;
     p5 = p5 + ypos*n5;
     p6 = p6 + zpos*n6;
-    std::vector<ROOT::Math::XYZPoint> p = {p1, p2, p3, p4, p5, p6};
-    std::vector<ROOT::Math::XYZVector> n = {n1, n2, n3, n4, n5, n6};
+    std::vector<Point> p = {p1, p2, p3, p4, p5, p6};
+    std::vector<Vector> n = {n1, n2, n3, n4, n5, n6};
     auto brik = new Threedpolygon(p, n);
     return(brik);
 };
 
 Threedpolygon* EdgeScintillator()
 {
-    ROOT::Math::XYZPoint p1(0,0,-2.5);
-    ROOT::Math::XYZPoint p2(0,0,2.5);
-    ROOT::Math::XYZPoint p3(3.15, 0, 0);
-    ROOT::Math::XYZPoint p4(-3.15, 0, -2.5);
-    ROOT::Math::XYZPoint p5(0, 3.105, 0);
-    ROOT::Math::XYZPoint p6(0, -3.105, 0);
+    Point p1(0,0,-2.5);
+    Point p2(0,0,2.5);
+    Point p3(3.15, 0, 0);
+    Point p4(-3.15, 0, -2.5);
+    Point p5(0, 3.105, 0);
+    Point p6(0, -3.105, 0);
 
-    ROOT::Math::XYZVector n1(0,0,1);
-    ROOT::Math::XYZVector n2(0,0,-1);
-    ROOT::Math::XYZVector n3(-1, 0, 0);
-    ROOT::Math::XYZVector n4(1, 0, 0);
+    Vector n1(0,0,1);
+    Vector n2(0,0,-1);
+    Vector n3(-1, 0, 0);
+    Vector n4(1, 0, 0);
     RotateVector(&n4, 0, -M_PI/14, 0);
-    ROOT::Math::XYZVector n5(0, -1, 0);
-    ROOT::Math::XYZVector n6(0, 1, 0);
+    Vector n5(0, -1, 0);
+    Vector n6(0, 1, 0);
 
-    std::vector<ROOT::Math::XYZPoint> p = {p1, p2, p3, p4, p5, p6};
-    std::vector<ROOT::Math::XYZVector> n = {n1, n2, n3, n4, n5, n6};
+    std::vector<Point> p = {p1, p2, p3, p4, p5, p6};
+    std::vector<Vector> n = {n1, n2, n3, n4, n5, n6};
     auto scintillator = new Threedpolygon(p, n);
     return(scintillator);
 };
 
 Threedpolygon* Backstop()
 {
-    ROOT::Math::XYZPoint p1(0,0,-2.5);
+    Point p1(0,0,-2.5);
     MovePoint(&p1, 0, 0, -5.1);
-    ROOT::Math::XYZPoint p2(0,0,2.5);
+    Point p2(0,0,2.5);
     MovePoint(&p2, 0, 0, -5.1);
-    ROOT::Math::XYZPoint p3(3.15+9.60, 0, 0);
-    ROOT::Math::XYZPoint p4(-3.15-9.60, 0, -2.5);
-    ROOT::Math::XYZPoint p5(0, 160, 0);
-    ROOT::Math::XYZPoint p6(0, -160, 0);
+    Point p3(3.15+9.60, 0, 0);
+    Point p4(-3.15-9.60, 0, -2.5);
+    Point p5(0, 160, 0);
+    Point p6(0, -160, 0);
 
-    ROOT::Math::XYZVector n1(0,0,1);
-    ROOT::Math::XYZVector n2(0,0,-1);
-    ROOT::Math::XYZVector n3(-1, 0, 0);
+    Vector n1(0,0,1);
+    Vector n2(0,0,-1);
+    Vector n3(-1, 0, 0);
     RotateVector(&n3, 0, +M_PI/14, 0);
-    ROOT::Math::XYZVector n4(1, 0, 0);
+    Vector n4(1, 0, 0);
     RotateVector(&n4, 0, -M_PI/14, 0);
-    ROOT::Math::XYZVector n5(0, -1, 0);
-    ROOT::Math::XYZVector n6(0, 1, 0);
+    Vector n5(0, -1, 0);
+    Vector n6(0, 1, 0);
 
-    std::vector<ROOT::Math::XYZPoint> p = {p1, p2, p3, p4, p5, p6};
-    std::vector<ROOT::Math::XYZVector> n = {n1, n2, n3, n4, n5, n6};
+    std::vector<Point> p = {p1, p2, p3, p4, p5, p6};
+    std::vector<Vector> n = {n1, n2, n3, n4, n5, n6};
     auto backstop = new Threedpolygon(p, n);
     return(backstop);
 };
@@ -232,7 +226,7 @@ class Tile
         Threedpolygon *m_sipm;
         std::vector<Particle *> m_particles;
     public:
-        Tile(ROOT::Math::XYZPoint position, double theta_x, double theta_y, double theta_z, bool is_edge)
+        Tile(Point position, double theta_x, double theta_y, double theta_z, bool is_edge)
         {
             Threedpolygon *scintillator;
             auto sipm = MakeBrick(3, 3, 0.1, 0, 0, -2.5);
@@ -281,7 +275,7 @@ class Tile
             return(m_sipm);
         };
 
-        void AddParticle(ROOT::Math::XYZPoint position, ROOT::Math::XYZVector velocity, double timedelay)
+        void AddParticle(Point position, Vector velocity, double timedelay)
         {
             auto particle = new Particle(position, velocity, timedelay);
             this->m_particles.push_back(particle);
@@ -325,28 +319,29 @@ class Tile
             */
 
             //added multithreading. works because all photons in the tile are independent (do not interact)
+            ///*
             tbb::parallel_for(size_t(0), m_particles.size(), [this, &time](size_t i) {
                 this->PathFindParticle(this->m_particles[i], time);
                 });
-
+            //*/
         };
 
         void PathFindParticle(Particle *particle, double time) //cannot do recursive function because of excessive use of pointers
         {
             if (!particle->IsAlive()) {return;};
-            ROOT::Math::XYZPoint projection = particle->Project(time);
+            Point projection = particle->Project(time);
             while (!m_scintillator->HasInsideStrict(&projection))
             {
                 Line line;
                 line.point = particle->Position();
                 line.vector = particle->Velocity();
-                ROOT::Math::XYZPoint bounce_point = m_scintillator->FirstPositiveIntersection(&line);
+                Point bounce_point = m_scintillator->FirstPositiveIntersection(&line);
                 double dt = sqrt(((bounce_point - particle->Position()).Mag2())/(particle->Velocity().Mag2()));
                 //inaccuracies in calculating dt may cause particle to be outside after moving by dt.
                 //to avoid this we use following while loop, which in extreme cases will even back the particle up using negative dt
                 //mostly affects small dt, so 2 reflections in a row, or iterations over small times such as by Animate()
                 double smallt = 0.00001*dt;
-                ROOT::Math::XYZPoint projection2 = particle->Project(dt);
+                Point projection2 = particle->Project(dt);
                 while (!m_scintillator->HasInsideStrict(&projection2))
                 {
                     dt = dt-smallt;
@@ -354,14 +349,14 @@ class Tile
                 };
                 particle->Move(dt);
                 ///*
-                ROOT::Math::XYZPoint temp_position = particle->Position();
+                Point temp_position = particle->Position();
                 if (m_sipm->HasInside(&temp_position))
                 {
                     particle->Kill();
                     break;
                 };
                 //*/
-                ROOT::Math::XYZVector normal = m_scintillator->Normal(&bounce_point);
+                Vector normal = m_scintillator->Normal(&bounce_point);
                 particle->Reflect(&normal);
                 time = time - dt;
                 if (time < 0) return;
@@ -466,13 +461,13 @@ void particles()
 
 ///*
     auto tile = new Tile(0,0,0,0,0,0, true);
-    double half_photons = 2500;
+    double half_photons = 25;
     std::default_random_engine generator;
     std::normal_distribution<double> distribution(0.0,1.0);
     for (int i = -half_photons; i < half_photons; ++i)
     {
-        ROOT::Math::XYZPoint position(1+0.5/(2*half_photons)*i,1+0.5/(2*half_photons)*i,1+0.5/(2*half_photons)*i);
-        ROOT::Math::XYZVector velocity(distribution(generator), distribution(generator), distribution(generator));
+        Point position(1+0.5/(2*half_photons)*i,1+0.5/(2*half_photons)*i,1+0.5/(2*half_photons)*i);
+        Vector velocity(distribution(generator), distribution(generator), distribution(generator));
         tile->AddParticle(position, 10*velocity.Unit(), (i+half_photons)/(2*half_photons)*10);
     };
 
@@ -489,12 +484,11 @@ void particles()
     };
     hist->Draw();
 
-
     auto tile2 = new Tile(0,0,0,0,0,0, false);
     for (int i = -half_photons; i < half_photons+1; ++i)
     {
-        ROOT::Math::XYZPoint position(1+0.5/(2*half_photons)*i,1+0.5/(2*half_photons)*i,1+0.5/(2*half_photons)*i);
-        ROOT::Math::XYZVector velocity(distribution(generator), distribution(generator), distribution(generator));
+        Point position(1+0.5/(2*half_photons)*i,1+0.5/(2*half_photons)*i,1+0.5/(2*half_photons)*i);
+        Vector velocity(distribution(generator), distribution(generator), distribution(generator));
         tile2->AddParticle(position, 10*velocity.Unit(), (i+half_photons)/(2*half_photons)*10);
     };
 
@@ -512,6 +506,7 @@ void particles()
     hist1->SetColors(kRed);
     hist1->Draw("same");
     mycanvas->SaveAs("./Signal.png");
+
 //*/
     /*
     auto a = MakeBrick(350,350,350, 0,0,0);
